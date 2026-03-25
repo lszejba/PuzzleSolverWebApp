de4b0db386c0bdf59f0ec4d4264bef8e150a43c5
lszejba <lukasz.szejba@gmail.com>
2026-03-25 23:45:21 +0100

    Local random sudoku generator added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import LinkButton from "../components/LinkButton";
+import { GenerateRandomSudoku } from "../utils/SudokuGenerator";
 
 function HomePage() {
   const navigate = useNavigate();
-  const beginnerSudokuState: string =
-    "859367401130508690062149385085931760000074058201850934000490813013002540540603279";
+  const randomSudokuState: string = GenerateRandomSudoku();
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh] gap-6">
       <h1>Select puzzle type:</h1>
@@ -12,7 +12,7 @@ function HomePage() {
         children="Sudoku"
         color="green"
         onClick={() =>
-          navigate("/sudoku", { state: { initialState: beginnerSudokuState } })
+          navigate("/sudoku", { state: { initialState: randomSudokuState } })
         }
       />
       <LinkButton
---
+++ src/utils/SudokuGenerator.ts
@@ -0,0 +1,15 @@
+import { getSudoku } from "sudoku-gen";
+
+export const GenerateRandomSudoku = (): string => {
+  const sudoku = getSudoku();
+  const result = sudoku.puzzle.replaceAll("-", "0");
+  return result;
+};
+
+export const GenerateSudoku = (
+  difficulty: "easy" | "medium" | "hard",
+): string => {
+  const sudoku = getSudoku(difficulty);
+  const result = sudoku.puzzle.replace("-", "0");
+  return result;
+};
